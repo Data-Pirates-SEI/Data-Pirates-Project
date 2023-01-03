0bd50cc53cc61cfcaff8f342def50e4c8a48fbe5
jhubbell89 <ewhs4jamesh@hotmail.com>
2023-01-03 09:52:36 -0800

    test

--- main_app/admin.py
+++ main_app/admin.py
@@ -3,6 +3,3 @@ from django.contrib import admin
 from .models import Chore, Comment, Supply
 
 # Register your models here.
-admin.site.register(Chore)
-admin.site.register(Comment)
-admin.site.register(Supply)
