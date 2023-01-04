0e4d00d5617acc220be8342fcff55e8ea9bdb897
ahmalla <68600561+ahmalla@users.noreply.github.com>
2023-01-04 15:50:57 -0800

    registering models in admin.py

--- main_app/admin.py
+++ main_app/admin.py
@@ -3,3 +3,6 @@ from django.contrib import admin
 from .models import Chore, Comment, Supply
 
 # Register your models here.
+admin.site.register(Chore)
+admin.site.register(Supply)
+admin.site.register(Comment)
