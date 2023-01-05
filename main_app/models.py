c3ceffea4d78f91bf170d180a14fc820b220a468
jhubbell89 <ewhs4jamesh@hotmail.com>
2023-01-05 14:28:28 -0800

    migrations

--- main_app/models.py
+++ main_app/models.py
@@ -8,9 +8,12 @@ from django.contrib.auth.models import User
 class Supply(models.Model):
     name = models.CharField(max_length=50)
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
     
 >>>>>>> 3ace633 (remove quantity and adding button icons)
+=======
+>>>>>>> 265dffb (migrations)
     def __str__(self):
         return self.name
 
